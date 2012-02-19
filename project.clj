eaa51cfc03353693c162418cd4f533ffe4e1b3ac
Nurullah Akkaya <nurullah@nakkaya.com>
2012-02-19 11:12:30 +0200

    add jvm opts

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-http "0.1.3"]]
+  :jvm-opts ["-Xmx16m"]
   :main easy-dns.core)
