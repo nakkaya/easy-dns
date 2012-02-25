5b741bd2c4910f5d37f57e01d76461345a41afcd
Nurullah Akkaya <nurullah@nakkaya.com>
2012-02-25 20:01:35 +0200

    add dep clojure 1.3

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject easy-dns "1.0.0-SNAPSHOT"
   :description "EasyDNS Dynamic DNS Updater"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [clj-http "0.1.3"]]
   :jvm-opts ["-Xmx16m"]
